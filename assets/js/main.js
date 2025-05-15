b9621e9bd27115daaa8804eaf367fe6a94f9217f
DA5100 <dramaarena5100@gontor.ac.id>
2025-05-15 16:29:48 +0700

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -218,11 +218,11 @@
 })();
 
 // Preloader
+  window.addEventListener('DOMContentLoaded', function () {
+    const preloader = document.querySelector('.preloader');
+    if (preloader) {
+      preloader.classList.add('hidden');
+    }
+  });
 
-window.addEventListener('load', function() {
-  const preloader = document.querySelector('.preloader');
-  if (preloader) {
-    preloader.style.display = 'none';
-  }
-}
 );
